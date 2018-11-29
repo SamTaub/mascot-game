4e9cd15375abc7b7d8cfc642428d771d72fb0233
Sam Taub <staub91@gmail.com>
2018-11-29 00:06:02 -0500

    initial character data

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+//Mascot Attributes
+
+//Swoop
+var swoopData = {
+    name: 'Swoop',
+    team: 'Eagles',
+    healthPoints: 152,
+    attackPower: 11,
+    counterAttackPower: 9,
+};
+
+//Franklin
+var franklinkData = {
+    name: 'Franklin',
+    team: '76ers',
+    healthPoints: 123,
+    attackPower: 21,
+    counterAttackPower: 13,
+}
+
+//Phanatic
+var phanaticData = {
+    name: 'Phillie Phanatic',
+    team: 'Phillies',
+    healthPoints: 140,
+    attackPower: 15,
+    counterAttackPower: 10,
+}
+
+//Gritty
+var grittyData = {
+    name: 'Gritty',
+    team: 'Flyers',
+    healthPoints: 175,
+    attackPower: 28,
+    counterAttackPower: 18,
+}
